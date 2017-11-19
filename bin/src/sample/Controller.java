b8a9c34c15de7e64500a5701e398617da3715281
Joaaofiilho <joaot10112@gmail.com>
2017-11-19 15:21:44 -0200

    Organizada as pastas Nesse commit organizei as pastas do programa para uma melhor organização na hora de programar. "Bin" e "Resources" criados.

---
+++ bin/src/sample/Controller.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+
+public class Controller {
+
+    @FXML
+    public Button btnPlayPause;
+    public Button btnStop;
+    public Button btnForward;
+    public Button btnBackward;
+
+    public Slider sldProgressBar;
+    public Slider sldVolumeBar;
+
+    public AnchorPane ancpBackground;
+
+
+    public void btnPlayPauseOnAction(ActionEvent event){
+
+    }
+    public void btnStopOnAction(ActionEvent event){
+
+    }
+    public void btnForwardOnAction(ActionEvent event){
+
+    }
+    public void btnBackwardOnAction(ActionEvent event){
+
+    }
+
+
+}
