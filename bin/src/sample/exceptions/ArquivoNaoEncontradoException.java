0e4cd479b336cfc458d0c217f87a09c6f8466f6f
Joao Filho <joa-ot@hotmail.com>
2017-11-19 17:41:37 -0300

    Criada algumas exceções como exemplo e guia.

---
+++ bin/src/sample/exceptions/ArquivoNaoEncontradoException.java
@@ -0,0 +1,22 @@
+package sample.exceptions;
+
+public class ArquivoNaoEncontradoException extends Exception {
+    //Como usar exceções
+    /*
+    public static void encontrarArquivo(File arquivo){
+        if(!arquivo.exists()){
+            throw new ArquivoNaoEncontradoException;
+        }else{
+            //O que quiser aqui
+        }
+    }
+     */
+
+    /*
+    try{
+        encontrarArquivo(arquivo);
+    }catch(ArquivoNaoEncontradoException e){
+        JOptionPane.showDialogMessage(null, "Arquivo não encontrado");
+    }
+     */
+}
