402b224999b4addb73313a87aaae82ae64df3343
Joaaofiilho <joaot10112@gmail.com>
2017-11-20 08:36:19 -0200

    Adicionado os métodos leitores e escritores de arquivos txt

--- bin/src/sample/Controller.java
+++ bin/src/sample/Controller.java
@@ -7,6 +7,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 
+import java.io.*;
+
 public class Controller {
 
     @FXML
@@ -21,18 +23,31 @@ public class Controller {
     public AnchorPane ancpBackground;
 
 
-    public void btnPlayPauseOnAction(ActionEvent event){
-
-    }
-    public void btnStopOnAction(ActionEvent event){
-
-    }
-    public void btnForwardOnAction(ActionEvent event){
-
-    }
-    public void btnBackwardOnAction(ActionEvent event){
+    public void btnPlayPauseOnAction(ActionEvent event) {
 
     }
 
+    public void btnStopOnAction(ActionEvent event) {
 
+    }
+
+    public void btnForwardOnAction(ActionEvent event) {
+
+    }
+
+    public void btnBackwardOnAction(ActionEvent event) {
+
+    }
+
+    //Métodos úteis
+    public static void definirEscritor(FileOutputStream arquivo, PrintWriter pr, String uri) throws IOException {
+        arquivo = new FileOutputStream(uri);
+        pr = new PrintWriter(arquivo);
+    }
+
+    public static void definirLeitor(FileInputStream arquivo, InputStreamReader input, BufferedReader br, String uri) throws IOException {
+        arquivo = new FileInputStream(uri);
+        input = new InputStreamReader(arquivo);
+        br = new BufferedReader(input);
+    }
 }
