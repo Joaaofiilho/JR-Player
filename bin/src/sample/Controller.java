0f67fe5fda6a32a4b663fa3f9d32c5eafbcbd6f2
Rubens Diniz <rubensadiniz@edu.unifor.br>
2017-11-21 16:27:00 -0200

    Commit do joão + meu commit
    v0.4.1

--- bin/src/sample/Controller.java
+++ bin/src/sample/Controller.java
@@ -1,7 +1,5 @@
 package sample;
 
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
@@ -20,11 +18,22 @@ public class Controller {
     public MediaPlayer mediaPlayer;
     public FileChooser fileChooser;
 
+    //Ler e escrever em arquivos de texto
+    public FileOutputStream arquivoO;
+    public PrintWriter pr;
+
+    public FileInputStream arquivoI;
+    public InputStreamReader input;
+    public BufferedReader br;
+
+    //Criar e copiar arquivos
+    public DataOutputStream data;
     @FXML
     public Button btnPlayPause;
     public Button btnStop;
     public Button btnForward;
     public Button btnBackward;
+    public Button btnAddMusic;
 
     public Slider sldProgressBar;
     public Slider sldVolumeBar;
@@ -32,28 +41,44 @@ public class Controller {
     public Label lblDisplay;
 
     public AnchorPane ancpBackground;
-    private final String songPath = "resources/songs/";
-    //FORMATO!!!!!!!!!!!!!!!
-    public String africaPath = songPath + "Africa.mp3";
+
+    public ListView lstvLista;
 
     public boolean isPlaying;
-    public String currentSong;
+
+    private final String songPath = "resources/songs/";
+    public String africaPath = songPath + "Africa.mp3";
 
     public Controller(){
         isPlaying = false;
         fileChooser = new FileChooser();
-
-        //Depois ajeitar o trycatch, não sei se fica necessário fazer isso aqui, pode ser no playPause msm...
-        //Se bem que seria util você colocar uma música inicial p/ prevenção de erros
-        //Ou carregar a pasta songs em vez de alguma playlist
         try {
+            //Depois ajeitar o trycatch, não sei se fica necessário fazer isso aqui, pode ser no playPause msm...
+            //Se bem que seria util você colocar uma música inicial p/ prevenção de erros
+            //Ou carregar a pasta songs em vez de alguma playlist
             media = new Media(new File(africaPath).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
         }catch (Exception e){
             System.out.println("Caminho não encontrado: " + e);
         }
 
-        //Tentei fazer uns bicho com volume não deu certo kOASDKOASDKOASKDOASKDOA
+        //Configurando os leitores e escritores de arquivos txt
+        try {
+            arquivoO = new FileOutputStream("");
+            pr = new PrintWriter(arquivoO);
+
+            arquivoI = new FileInputStream("");
+            input = new InputStreamReader(arquivoI);
+            br = new BufferedReader(input);
+        }catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado");
+        }
+
+        //Definindo que tipo de arquivos o FileChooser pode pegar
+        //fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivos MP3", "*.mp3"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Arquivo MP3", "*.mp3"));
+
+        //Tentei fazer uns bicho com volume não deu certo Kappa
 //        sldVolumeBar = new Slider();
 //        sldVolumeBar.setValue(mediaPlayer.getVolume() * 50);
 //        sldVolumeBar.valueProperty().addListener(new InvalidationListener() {
@@ -66,7 +91,7 @@ public class Controller {
 
     public void btnPlayPauseOnAction(ActionEvent event) {
         try {
-            lblDisplay.setText("Now playing: " +  media.getSource().substring(64));
+            lblDisplay.setText("Now playing: " +  media.toString().substring(64));
             if (isPlaying) {
                 mediaPlayer.pause();
                 isPlaying = false;
@@ -97,16 +122,27 @@ public class Controller {
 
     }
 
-    //Métodos úteis
-    public static void definirEscritor(FileOutputStream arquivo, PrintWriter pr, String uri) throws IOException {
-        arquivo = new FileOutputStream(uri);
-        pr = new PrintWriter(arquivo);
+    public void btnAddMusicOnAction(ActionEvent event){
+        File file = fileChooser.showOpenDialog(null);
+        //System.out.println(file.getName());
     }
 
-    public static void definirLeitor(FileInputStream arquivo, InputStreamReader input, BufferedReader br, String uri) throws IOException {
-        arquivo = new FileInputStream(uri);
-        input = new InputStreamReader(arquivo);
-        br = new BufferedReader(input);
+    //Métodos úteis
+
+    public void definirLeitor(String uri){
+        try {
+            arquivoO = new FileOutputStream(new File(uri).toURI().toString());
+        }catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado!");
+        }
+    }
+
+    public void definirEscritor(String uri){
+        try {
+            arquivoI = new FileInputStream(new File(uri).toURI().toString());
+        }catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado!");
+        }
     }
 
 }
