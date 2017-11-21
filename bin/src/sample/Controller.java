da64861e7c6ccc9e1629b105cd4f1f3a83c6a5bc
RubensDiniz <rubensadiniz@edu.unifor.br>
2017-11-21 11:00:40 -0200

    Adicionada a melhor musica e feitas algumas instancias

--- bin/src/sample/Controller.java
+++ bin/src/sample/Controller.java
@@ -3,13 +3,24 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+
 
 import java.io.*;
 
 public class Controller {
+
+    private final String songPath = "resources/songs/";
+    //FORMATO!!!!!!!!!!!!!!!
+    //teste
+    public String africaPath = songPath + "Africa.mp3";
+    public Media media;
+    public MediaPlayer mediaPlayer;
+    public FileChooser fileChooser;
 
     @FXML
     public Button btnPlayPause;
@@ -20,19 +31,42 @@ public class Controller {
     public Slider sldProgressBar;
     public Slider sldVolumeBar;
 
+    public Label lblDisplay;
+
     public AnchorPane ancpBackground;
 
+    public Controller(){
+        isPlaying = false;
+        fileChooser = new FileChooser();
+        try {
+            media = new Media(new File(africaPath).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+        }catch (Exception e){
+            System.out.println("Caminho não encontrado: " + e);
+        }
+
+    }
 
     public void btnPlayPauseOnAction(ActionEvent event) {
+        lblDisplay.setText("Now playing:" +  media.toString());
 
+        if (isPlaying){
+            mediaPlayer.pause();
+            isPlaying = false;
+        } else {
+            mediaPlayer.play();
+            isPlaying = true;
+        }
     }
 
     public void btnStopOnAction(ActionEvent event) {
-
+        mediaPlayer.stop();
+        isPlaying = false;
     }
 
     public void btnForwardOnAction(ActionEvent event) {
-
+//        media = new Media(new File(fileChooser.showOpenDialog());
+//        mediaPlayer = new MediaPlayer(media);
     }
 
     public void btnBackwardOnAction(ActionEvent event) {
@@ -50,4 +84,6 @@ public class Controller {
         input = new InputStreamReader(arquivo);
         br = new BufferedReader(input);
     }
+
+    public boolean isPlaying;
 }
--- bin/src/sample/Main.java
+++ bin/src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JR-Player");
-        primaryStage.setScene(new Scene(root, 500, 300));
+        primaryStage.setScene(new Scene(root, 500, 360));
         primaryStage.setResizable(false);
         try {
             primaryStage.getIcons().add(new Image(new File("resources/images/icon2.jpg").toURI().toString()));
